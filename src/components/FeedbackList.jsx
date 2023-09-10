c9217a9d4dee0936db00c17c97efa7bb39810175
itisdanish <ardentdew@gmail.com>
2023-09-10 18:20:51 +0530

    Input Update
    Update

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,21 +1,43 @@
 import React from 'react'
 import FeedbackItems from './FeedbackItems'
+import {motion, AnimatePresence} from 'framer-motion'
 import PropTypes from 'prop-types'
 
 function FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0){
       return <p>No Feedback yet</p>
     }
-  return (
-    <div className='feedback-list'>
-      {feedback.map((item)=>(
-       <FeedbackItems 
-        key={item.id} 
-        item={item} 
-        handleDelete ={handleDelete}/>
-      ))}
-    </div>
-  )
+    return (
+      <div className='feedback-list'>
+        <AnimatePresence>
+        {feedback.map((item)=>(
+         <motion.div 
+         key={item.id}
+         initial={{opacity:0}}
+         animate={{opacity:1}}
+         exit={{opacity:0}}
+         >
+         <FeedbackItems 
+          key={item.id} 
+          item={item} 
+          handleDelete ={handleDelete}/>
+          </motion.div>
+        ))}
+        </AnimatePresence>
+      </div>
+    )
+ 
+
+    // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item)=>(
+  //      <FeedbackItems 
+  //       key={item.id} 
+  //       item={item} 
+  //       handleDelete ={handleDelete}/>
+  //     ))}
+  //   </div>
+  // )
 }
 
 FeedbackList.prototype={
